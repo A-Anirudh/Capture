28f0f0c58c2fa37f84605504260dd5df3d8d46b4
A-Anirudh <70565463+A-Anirudh@users.noreply.github.com>
2021-08-02 17:56:28 +0530

    faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSections from '../components/FaqSections'
 
 const AboutUs = () => {
     return (
         <div>
             <AboutSection />
             <ServicesSection />
+            <FaqSections />
 
 
         </div>
